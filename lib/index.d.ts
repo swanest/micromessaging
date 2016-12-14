525c8c2b9fc751645f8c0549e1afe720660ab4b6
Cyril Lequeux <l.cyril@gmail.com>
2016-12-14 17:15:52 +0100

    break(index): exports an object instead of a function fix(ts): improved ts integration

---
+++ lib/index.d.ts
@@ -0,0 +1,187 @@
+/*
+ Data-structures
+ */
+
+declare namespace Types {
+    type event = "unroutableMessage" | "unhandledMessage" | "closed" | "failed" | "unreachable";
+    type exchange = "direct" | "fanout" | "topic";
+    type queue = "direct" | "fanout" | "topic";
+}
+
+declare namespace Config {
+
+    interface Exchange {
+        name?: string;
+        type?: Types.exchange;
+        autoDelete?: boolean;
+        durable?: boolean;
+        persistent?: boolean;
+        alternate?: string;
+        publishTimeout?: number;
+        replyTimeout?: number;
+        limit?: number;
+    }
+
+    interface Queue {
+        name?: string;
+        autoDelete?: boolean;
+        durable?: boolean;
+        exclusive?: boolean;
+        subscribe?: boolean;
+        limit?: number;
+        noAck?: number;
+        noBatch?: number;
+        queueLimit?: number;
+        messageTtl?: number;
+        expires?: number;
+        deadLetter?: string;
+        maxPriority?: number;
+        unique?: "hash" | "id" | "consistent";
+    }
+
+    interface MemoryPressure {
+        memoryThreshold?: number;
+        interval?: number;
+        stillUnderPressure?: number;
+        consecutiveGrowths?: number;
+    }
+
+    interface Setup {
+        discoverable?: boolean;
+        memoryPressureHandled?: boolean | MemoryPressure
+        config?: {
+            EXCHANGE_MESSAGES?: Exchange;
+            EXCHANGE_REQUESTS?: Exchange;
+            EXCHANGE_DEAD_REQUESTS?: Exchange;
+            Q_MESSAGES?: Queue;
+            Q_SHARED_MESSAGES?: Queue;
+            Q_RESPONSES?: Queue;
+            Q_REQUESTS?: Queue;
+            Q_DEAD_REQUESTS?: Queue;
+        }
+    }
+
+    interface Emit {
+        timeout?: number;
+        expiresAfter?: number;
+        isPublic?: boolean;
+    }
+
+    interface ScopeEmit {
+        timeout?: number;
+        expiresAfter?: number;
+    }
+
+    interface Request {
+        timeout?: number;
+        expiresAfter?: number;
+        replyTimeout?: number;
+    }
+
+    interface Task {
+        timeout?: number;
+        expiresAfter?: number;
+    }
+
+}
+
+/*
+ Interfaces
+ */
+
+interface ListenHandleResult {
+    onError: (cb: Function) => {
+        remove: () => When.Promise<any>,
+        promise: When.Promise<any>
+    };
+    remove: () => When.Promise<any>;
+    promise: When.Promise<any>;
+}
+
+interface Message {
+    type: string;
+    body: any;
+    properties: {
+        isRedelivered: boolean;
+        exchange: string;
+        queue: undefined | string;
+        routingKey: string;
+        path: string;
+        id?: string;
+        correlatedTo?: string
+        contentType: string;
+        contentEncoding: string;
+        expiresAfter: number;
+        timestamp: number;
+        replyTo?: {
+            exchange: '';
+            queue: string;
+            routingKey: string;
+            path: string;
+        }
+    };
+    status: "PENDING" | "ACKED" | "NACKED" | "REJECTED";
+    //Requests
+    write(body: any, headers?: any): void;
+    end(body: any, headers?: any): void;
+    reply(body: any, headers?: any): void;
+    reject(): void; //if it is a request, then client won't receive any response
+    ack(): void; //only for tasks
+    nack(): void;
+}
+
+interface ScopeEmit{
+    emit(serviceName: string, route: string, body: any, headers?: any, opts?: Config.ScopeEmit): When.Promise<void>;
+}
+
+interface ScopeListen{
+    listen(route: string, cb: (mesage: Message) => void, serviceName?: string): ListenHandleResult;
+}
+
+export declare class Service {
+    name: string;
+    uniqueID: string;
+    replications: Array<any>;
+    isElected: boolean;
+
+    // Are these necessary ?
+    // noCheck: boolean;
+    // queueName: string;
+    // exchange: string;
+
+    constructor(name: string, setupOpts?: Config.Setup);
+
+
+    on(event: Types.event, cb?: (message: Message | Error) => void): void;
+
+    once(event: Types.event, cb?: (message: Message | Error) => void): void;
+
+    close(): When.Promise<void>;
+
+    connect(uri?: string): When.Promise<void>;
+
+    subscribe(): When.Promise<void>;
+
+    emit(serviceName: string, route: string, body: any, headers?: any, opts?: Config.Emit): When.Promise<void>;
+
+    publicly: ScopeEmit;
+    privately: ScopeEmit;
+
+    request(serviceName: string, taskName: string, body: any, headers?: any, opts?: Config.Request): When.Promise<Message>;
+
+    task(serviceName: string, taskName: string, body: any, headers?: any, opts?: Config.Task): When.Promise<void>;
+
+    notify(serviceName: string, taskName: string, body: any, headers?: any, opts?: Config.Task): When.Promise<void>;
+
+    listen(route: string, cb: (mesage: Message) => void, serviceName?: string): ListenHandleResult;
+
+    exclusively: ScopeListen;
+    death: ScopeListen;
+
+    handle(taskName: string, cb: (mesage: Message) => void): ListenHandleResult;
+
+    prefetch(count: number): When.Promise<void>;
+
+    getRequestReport(serviceName: string): When.Promise<{queueSize: number;}>;
+
+}
--- typings-tests/micromessaging-tests.ts
+++ typings-tests/micromessaging-tests.ts
@@ -1,36 +1,47 @@
-/// <reference path="../micromessaging.d.ts" />
-const when = require('when'),
-    Service = require('../lib'),
-    tx = new Service('sender'),
-    rx = new Service('abc');
+///<reference path="../node_modules/@types/node/index.d.ts"/>
+import {Service, Message} from "../lib";
+import * as When from 'when';
 
-async function test() {
-    await when.all([tx.connect(), rx.connect()]);
-
-    rx.listen('stock.aapl.split', function (message: Object) {
-        console.log('stock.aapl.split', message);
+const tx = new Service('sender'),
+    rx = new Service('abc', {
+        discoverable: false
     });
 
-    rx.listen('stock.msft.*', function (message: Object) {
-        console.log('stock.msft.*', message);
+async function test() {
+    await When.all([tx.connect(), rx.connect()]);
+
+    rx.listen('stock.aapl.split', function (message: Message) {
+        console.log('stock.aapl.split', message.body);
+    });
+
+    rx.listen('stock.msft.*', function (message: Message) {
+        console.log('stock.msft.*', message.body);
+    });
+
+    rx.exclusively.listen("stock.msft.*", function (mesage: Message) {
+        console.log(mesage.body);
     });
 
     await rx.subscribe();
 
-    await tx.emit('abc', 'stock.aapl.split', {ratio: '7:1'}, {headerAppInfo: 'test'}, {timeout:300, expiresAfter:4000});
+    await tx.emit('abc', 'stock.aapl.split', {ratio: '7:1'}, {headerAppInfo: 'test'}, {
+        timeout: 300,
+        expiresAfter: 4000
+    });
     await tx.emit('abc', 'stock.msft.split', {ratio: '3:1'});
     await tx.emit('abc', 'stock.msft.cashDiv', {amount: 0.72});
 
-    await when.all([tx.close(), rx.close()]);
+    await When.all([tx.close(), rx.close()]);
 }
+
 
 tx.on('unreachable', () => {
     console.warn(tx.name + ' failed to reach rabbit server');
 });
-tx.on('unroutableMessage', (message: Object) => {
+tx.on('unroutableMessage', (message: Message) => {
     console.warn(tx.name + ' encountered an unroutable message', message);
 });
-tx.on('unhandledMessage', (message: Object) => {
+tx.on('unhandledMessage', (message: Message) => {
     console.warn(tx.name + ' encountered an unhandled message', message);
 });
 tx.on('closed', () => {
